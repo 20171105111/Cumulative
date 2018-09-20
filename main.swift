78138fc3ea0f0355cd1bce4afd670b9c79ad3620
20171105111 <2604406667@qq.com>
2018-09-20 09:34:47 +0800

    累加

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Cumulative
+//
+//  Created by 徐涛 on 2018/9/20.
+//  Copyright © 2018年 徐涛. All rights reserved.
+//
+
+import Foundation
+var sum = 0
+for i in 1...100{
+    sum = sum + i;
+}
+print(sum)
+
